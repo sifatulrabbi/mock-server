e069ec8d3b0bb35009e75216cf851596900f53fd
Sifatul Rabbi <mdsifatulislam.rabbi@gmail.com>
2022-11-02 20:52:53 +0600

    upload to github

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -5,7 +5,7 @@ import { AppService } from './app.service';
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  @Get()
+  @Get('/mock-server')
   getHello(): string {
     // const ping =
     return this.appService.getHello();
--- src/app.module.ts
+++ src/app.module.ts
@@ -1,16 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { MongooseModule } from '@nestjs/mongoose';
-import { CatsModule } from './cat/cat.module';
 import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [
-    ConfigModule.forRoot({ envFilePath: '.env' }),
-    MongooseModule.forRoot(process.env.MONGODB_URI),
-    CatsModule,
-  ],
+  imports: [ConfigModule.forRoot({ envFilePath: '.env' })],
   controllers: [AppController],
   providers: [AppService],
 })
